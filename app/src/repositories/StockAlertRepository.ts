b7c0f0eb6f6952c0e089713cdb92ba9790fcdd9c
Matthew <mattjarman22@outlook.com>
2021-03-20 18:58:51 +0000

    Add StockAlertRepository tests

--- app/src/repositories/StockAlertRepository.ts
+++ app/src/repositories/StockAlertRepository.ts
@@ -1,8 +1,9 @@
 import {
-  CreateTableCommand, DeleteItemCommand, DeleteItemCommandInput, CreateTableCommandInput,
-  DeleteTableCommand, GetItemCommand, GetItemCommandInput,
-  ListTablesCommand,
-  UpdateItemCommand, UpdateItemCommandInput
+  GetItemCommand,
+  GetItemCommandInput,
+  UpdateItemCommand,
+  UpdateItemCommandInput,
+  UpdateItemOutput
 } from '@aws-sdk/client-dynamodb'
 import { unmarshall } from '@aws-sdk/util-dynamodb'
 import { StockAlert } from '../interfaces/repositories/StockAlertRepository'
@@ -14,7 +15,7 @@ export default class StockAlertRepository extends DB {
   public constructor () {
     super()
     if (!process.env.STOCK_ALERTS_TABLE_NAME) {
-      throw new Error('Table name Stock Alerts is not set.')
+      throw new Error('Stock Alerts table name is not set.')
     }
 
     this.tableName = process.env.STOCK_ALERTS_TABLE_NAME
@@ -42,23 +43,7 @@ export default class StockAlertRepository extends DB {
     return unmarshall(Item) as StockAlert
   }
 
-  public async delete (product: string, source: string): Promise<unknown> {
-    const params: DeleteItemCommandInput = {
-      TableName: this.tableName,
-      Key: {
-        product: {
-          S: product
-        },
-        source: {
-          S: source
-        }
-      }
-    }
-
-    return this.db.send(new DeleteItemCommand(params))
-  }
-
-  public async update (product: string, source: string, lastSent: Date = new Date()): Promise<unknown> {
+  public async update (product: string, source: string, lastSent: Date = new Date()): Promise<UpdateItemOutput> {
     const params: UpdateItemCommandInput = {
       TableName: this.tableName,
       Key: {
@@ -78,45 +63,5 @@ export default class StockAlertRepository extends DB {
     }
 
     return this.db.send(new UpdateItemCommand(params))
-  }
-
-  public async listTables (): Promise<unknown> {
-    return this.db.send(new ListTablesCommand({}))
-  }
-
-  public async createTable (): Promise<unknown> {
-    const params: CreateTableCommandInput = {
-      TableName: 'ddb-l-ew1-stock-alerts',
-      AttributeDefinitions: [
-        {
-          AttributeName: 'product',
-          AttributeType: 'S'
-        },
-        {
-          AttributeName: 'source',
-          AttributeType: 'S'
-        }
-      ],
-      KeySchema: [
-        {
-          AttributeName: 'product',
-          KeyType: 'HASH'
-        },
-        {
-          AttributeName: 'source',
-          KeyType: 'RANGE'
-        }
-      ],
-      ProvisionedThroughput: {
-        ReadCapacityUnits: 1,
-        WriteCapacityUnits: 1
-      }
-    }
-
-    return this.db.send(new CreateTableCommand(params))
-  }
-
-  public async deleteTable (): Promise<unknown> {
-    return this.db.send(new DeleteTableCommand({ TableName: this.tableName }))
   }
 }
---
+++ app/test/unit/repositories/StockAlertRepository.test.ts
@@ -0,0 +1,103 @@
+import MockDate from 'mockdate'
+import { GetItemOutput, UpdateItemCommand } from '@aws-sdk/client-dynamodb'
+import StockAlertRepository from '../../../src/repositories/StockAlertRepository'
+
+const testDate = new Date('2021-01-01')
+MockDate.set(testDate)
+
+const sendMock = jest.fn()
+jest.mock('@aws-sdk/client-dynamodb', () => ({
+  DynamoDBClient: jest.fn().mockImplementation(() => ({
+    send: sendMock
+  })),
+  GetItemCommand: jest.fn(),
+  UpdateItemCommand: jest.fn()
+}))
+
+describe('Test StockAlertRepository', () => {
+  beforeEach(() => {
+    process.env.STOCK_ALERTS_TABLE_NAME = 'ddb-t-ew1-stock-alerts'
+  })
+
+  describe('.constructor()', () => {
+    test('It throws an error if the STOCK_ALERTS_TABLE_NAME env variable is not defined', () => {
+      expect(() => {
+        process.env.STOCK_ALERTS_TABLE_NAME = ''
+
+        // eslint-disable-next-line no-new
+        new StockAlertRepository()
+      }).toThrow(new Error('Stock Alerts table name is not set.'))
+    })
+  })
+
+  describe('.get()', () => {
+    test('It formats and returns the StockAlert on a successful request', async () => {
+      const output: GetItemOutput = {
+        ConsumedCapacity: undefined,
+        Item: {
+          last_sent: { S: '2021-03-20T18:36:05.851Z' },
+          product: { S: 'PS5' },
+          source: { S: 'Amazon' }
+        }
+      }
+
+      sendMock.mockReturnValue(output)
+
+      const repo = new StockAlertRepository()
+      const actual = await repo.get('PS5', 'Amazon')
+
+      expect(actual).toEqual({
+        product: 'PS5',
+        source: 'Amazon',
+        last_sent: '2021-03-20T18:36:05.851Z'
+      })
+    })
+
+    test('It returns false if the StockAlert was not found', async () => {
+      const output: GetItemOutput = {
+        ConsumedCapacity: undefined,
+        Item: undefined
+      }
+
+      sendMock.mockReturnValue(output)
+
+      const repo = new StockAlertRepository()
+      const actual = await repo.get('PS5', 'Amazon')
+
+      expect(actual).toEqual(false)
+    })
+  })
+
+  describe('.update()', () => {
+    test('It updates the last_sent date of a StockAlert with a provided date', async () => {
+      const repo = new StockAlertRepository()
+      const lastSent = new Date('2020-01-01')
+      await repo.update('PS5', 'Amazon', lastSent)
+
+      expect(UpdateItemCommand).toHaveBeenCalledWith(
+        expect.objectContaining({
+          ExpressionAttributeValues: {
+            ':last_sent': {
+              S: lastSent.toISOString()
+            }
+          }
+        })
+      )
+    })
+
+    test('It updates the last_sent date of a StockAlert to now if no date is provided', async () => {
+      const repo = new StockAlertRepository()
+      await repo.update('PS5', 'Amazon')
+
+      expect(UpdateItemCommand).toHaveBeenCalledWith(
+        expect.objectContaining({
+          ExpressionAttributeValues: {
+            ':last_sent': {
+              S: testDate.toISOString()
+            }
+          }
+        })
+      )
+    })
+  })
+})
