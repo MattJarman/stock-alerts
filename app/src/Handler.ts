f9f50647f83c16f100ccdbc5db19743889f98646
Matthew <mattjarman22@outlook.com>
2021-05-10 23:28:36 +0100

    Add Handler tests

--- app/src/Handler.ts
+++ app/src/Handler.ts
@@ -1,13 +1,8 @@
 import { SourceResult } from './interfaces/sources/Source'
-import Nvidia from './sources/Nvidia'
+import sources from './Sources'
 
 export const handler = async (): Promise<boolean> => {
-  const source = new Nvidia({
-    productName: '3080',
-    productUrl: 'geforce/store/gpu/?page=1&limit=9&locale=en-gb&category=GPU&gpu=RTX 3080'
-  })
-
-  const promises: Promise<SourceResult>[] = [source.find().finally(() => source.close())]
+  const promises: Promise<SourceResult>[] = sources.map(source => source.find().finally(() => source.close()))
   const results = await Promise.all(promises)
 
   // TODO: Send SNS notification
--- app/test/unit/Handler.test.ts
+++ app/test/unit/Handler.test.ts
@@ -1,34 +1,87 @@
 import { handler } from '../../src/Handler'
+import sources from '../../src/Sources'
+import Amazon from '../../src/sources/Amazon'
 import Nvidia from '../../src/sources/Nvidia'
+import Source from '../../src/sources/Source'
 import Mock = jest.Mock;
 
-const nvidiaFindMock = jest.fn()
-const NvidiaMock = Nvidia as Mock
-jest.mock('../../src/sources/Nvidia')
-NvidiaMock.mockImplementation(() => ({
-  find: nvidiaFindMock
+let sourcesMock = sources as unknown as Array<Source>
+jest.mock('../../src/Sources', () => [])
+
+const SourceMock = Source as unknown as Mock
+jest.mock('../../src/sources/Source')
+const findMock = jest.fn()
+const closeMock = jest.fn()
+SourceMock.mockImplementation(() => ({
+  find: findMock,
+  close: closeMock
 }))
 
 describe('Test Handler', () => {
-  test('The handler returns true if stock was found for at least one product', async () => {
-    nvidiaFindMock.mockResolvedValue({
+  afterEach(() => {
+    sourcesMock = []
+  })
+
+  it('returns true if stock was found for at least one product', async () => {
+    const sources = [
+      new Nvidia({
+        productName: '3080',
+        productUrl: '3080'
+      }),
+      new Amazon({
+        productName: 'PS5',
+        productUrl: ''
+      })
+    ]
+
+    findMock.mockResolvedValueOnce({
       product: '3080',
-      url: 'https://nvidia-3080-test.com',
+      url: 'https://shop.nvidia.com/en-gb/3080',
       inStock: true
     })
 
-    const actual = await handler()
-    expect(actual).toEqual(true)
-  })
-
-  test('The handler returns false if stock was not found for any product', async () => {
-    nvidiaFindMock.mockResolvedValue({
-      product: '3080',
-      url: 'https://nvidia-3080-test.com',
+    findMock.mockResolvedValueOnce({
+      product: 'PS5',
+      url: 'https://www.amazon.co.uk/PS5',
       inStock: false
     })
 
+    sources.forEach(source => sourcesMock.push(source))
+    const actual = await handler()
+    expect(actual).toEqual(true)
+
+    expect(closeMock).toHaveBeenCalledTimes(sources.length)
+  })
+
+  it('returns false if no stock was found for any product', async () => {
+    const sources = [
+      new Nvidia({
+        productName: '3080',
+        productUrl: '3080'
+      }),
+      new Amazon({
+        productName: 'PS5',
+        productUrl: ''
+      })
+    ]
+
+    findMock.mockResolvedValueOnce({
+      product: '3080',
+      url: 'https://shop.nvidia.com/en-gb/3080',
+      inStock: false
+    })
+
+    findMock.mockResolvedValueOnce({
+      product: 'PS5',
+      url: 'https://www.amazon.co.uk/PS5',
+      inStock: false
+    })
+
+    sources.forEach(source => sourcesMock.push(source))
+
     const actual = await handler()
     expect(actual).toEqual(false)
+
+    expect(closeMock).toHaveBeenCalledTimes(sources.length)
   })
 })
