1737b17aad8a8844030cb3994a849b28325294f9
Matthew <mattjarman22@outlook.com>
2021-06-25 12:22:09 +0100

    Use .buy-link selector

--- app/src/sources/Nvidia.ts
+++ app/src/sources/Nvidia.ts
@@ -2,6 +2,6 @@ import Source from './Source'
 
 export default class Nvidia extends Source {
   readonly baseUrl = 'https://shop.nvidia.com/en-gb'
-  readonly defaultSelector: string = '.stock-grey-out'
-  readonly defaultSelectorEvaluation: boolean = false
+  readonly defaultSelector: string = '.buy-link'
+  readonly defaultSelectorEvaluation: boolean = true
 }
